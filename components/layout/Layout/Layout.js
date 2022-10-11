c4a05987724ae5806bbb560dfaf2cb76f3aa6be9
Agustin Quevedo <agustin.quevedo@monstar-lab.com>
2022-10-11 15:47:31 +0200

    fetch pokemon list

---
+++ components/layout/Layout/Layout.js
@@ -0,0 +1,7 @@
+export default function Layout({ children }) {
+  return (
+    <div className="w-full max-w-7xl sm:w-[90%]  mx-[auto] my-0 px-4 md:px-6 lg:px-8">
+      {children}
+    </div>
+  );
+}
